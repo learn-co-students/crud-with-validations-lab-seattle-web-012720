5688eae603b51ec2ea912f693459d07a161d5236
Agandaur <cjwagstaffe@gmail.com>
2020-02-27 11:31:38 -0800

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "All These Things I Have Done", released: true, release_year: 2009, artist_name: "The Killers", genre: "Alt Rock")
+Song.create(title: "Yellow", released: true, release_year: 2001, artist_name: "Coldplay", genre: "Pop")
+Song.create(title: "Paint It Black", released: true, release_year: 1966, artist_name: "The Rolling Stones", genre: "Rock")
